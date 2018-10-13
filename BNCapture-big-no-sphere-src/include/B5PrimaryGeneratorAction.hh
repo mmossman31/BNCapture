e07eda97aeb8781e2d2ff553b819f12a69321ec0
mmossman31 <43974225+mmossman31@users.noreply.github.com>
2018-10-13 18:22:16 -0400

    Add files via upload

---
+++ BNCapture-big-no-sphere-src/include/B5PrimaryGeneratorAction.hh
@@ -0,0 +1,58 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B5PrimaryGeneratorAction.hh 101036 2016-11-04 09:00:23Z gcosmo $
+//
+/// \file B5PrimaryGeneratorAction.hh
+/// \brief Definition of the B5PrimaryGeneratorAction class
+
+#ifndef B5PrimaryGeneratorAction_h
+#define B5PrimaryGeneratorAction_h 1
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "globals.hh"
+
+//Note swap G4ParticleGun for G4GeneralParticleSource
+
+class G4GeneralParticleSource;
+class G4Event;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class B5PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
+{
+public:
+	B5PrimaryGeneratorAction();
+	~B5PrimaryGeneratorAction();
+
+	virtual void GeneratePrimaries(G4Event*);
+
+private:
+	G4GeneralParticleSource* fParticleGun;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
